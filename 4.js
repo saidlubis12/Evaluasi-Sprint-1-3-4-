c8a4d663283cce06bf4a22f764c535ef51717ad7
ninjaa <alkhudrisaid83@gmail.com>
2020-09-26 13:42:42 +0700

    first commit

---
+++ 4.js
@@ -0,0 +1,19 @@
+function mentorProgrammer(nama, asal, status) {
+    nama();
+    asal();
+    status();
+
+}
+
+function nama() {
+    console.log("nama mentor wandi pratama")
+}
+
+function asal() {
+    console.log("berasal dari Prabumulih")
+}
+
+function status() {
+    console.log("statusnya santri")
+}
+mentorProgrammer(nama, asal, status);
